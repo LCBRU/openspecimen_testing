baee15e026268e1adf94164c3641ef0faa70f522
Richard Bramley <rabramley@gmail.com>
2021-07-29 08:49:27 +0100

    Refactor

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 import logging
 import os
 from open_specimen_tester import OpenSpecimenSeleniumTestHelper
-from site_tester import SiteTester
-from user_tester import UserTester
-from role_tester import RoleTester
-from query_tester import QueryTester
-from order_tester import OrderTester
-from job_tester import JobTester
-from institute_tester import InstituteTester
-from form_tester import FormTester
-from distribution_protocol_tester import DistributionProtocolTester
-from container_tester import ContainerTester
-from collection_protocol_tester import CollectionProtocolTester
-from cart_tester import CartTester
+from non_destructive_tests.site_tester import SiteTester
+from non_destructive_tests.user_tester import UserTester
+from non_destructive_tests.role_tester import RoleTester
+from non_destructive_tests.query_tester import QueryTester
+from non_destructive_tests.order_tester import OrderTester
+from non_destructive_tests.job_tester import JobTester
+from non_destructive_tests.institute_tester import InstituteTester
+from non_destructive_tests.form_tester import FormTester
+from non_destructive_tests.distribution_protocol_tester import DistributionProtocolTester
+from non_destructive_tests.container_tester import ContainerTester
+from non_destructive_tests.collection_protocol_tester import CollectionProtocolTester
+from non_destructive_tests.cart_tester import CartTester
 from dotenv import load_dotenv
 from datetime import datetime
 
